da3c6a79cfbd1ee77d892b0a9ce4ac1d45a98352
Jennifer Patton <jenbuckleypatton@gmail.com>
2023-09-21 20:26:46 -0700

    creating a searchbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 //imports goes here
+import React, {useState} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SearchBar from './components/searhbar';
+
 
 const { Routes } = require("react-router-dom");
 
 
 function App() {
+    const [data, setData] = useState([]);
+    const [filteredData, setFilteredData] = useState(data);
+
+    const handleSearch = (searchTerm) => {
+        const filftered = data.filter((item) => 
+        item.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredData(filtered);
+    }
 
     return (
         <div className="App">
+            <div className="SearchBar">
+                <SearchBar handleSearch={handleSearch} />
+                <ul>
+                    {filteredData.map((item) => (
+                        <li key={item.id}>{item.name}</li>
+                    ))}
+                </ul>
+            </div>
             <Router>
-            <Routes>
-                <Route path= />
-                <Route path= />
-                <Route path= />
-            </Routes>
+                <Routes>
+                    <Route path= />
+                    <Route path= />
+                    <Route path= />
+                </Routes>
             </Router>
         </div>
     );
