dfa998957432fe372189a0093be5003ee6bdfb2c
Jennifer Patton <jenbuckleypatton@gmail.com>
2023-09-21 19:50:20 -0700

    making product dummydata

---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,44 @@
+
+
+function product(){
+const dummyData = [
+    {
+        id: 1,
+        title: "Pokemon-go",
+        year: "2015",
+        platform: "mobile",
+        price: "10.99",
+    },
+    {
+        id: 2,
+        title: "Pay-Day 3",
+        year: "2023",
+        platform: "all",
+        price: "69.99",
+    },
+    {
+        id: 3,
+        title: "Phasmophobia",
+        year: "2020",
+        platform: "PC",
+        price: "15.00",
+    },
+    {
+        id: 4,
+        title: "Fable 3",
+        year: "2010",
+        platform: "xBox",
+        price: "6.99",
+    },
+    {
+        id: 5,
+        title: "Final Fantasy VII",
+        year: "1997",
+        platform: "playstation",
+        price: "24.99",
+    },
+]
+    return ()
+}
+
+export default product;
