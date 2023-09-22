d2012e11fe5a6595278503bc3660bb77cbd0fbf1
Jennifer Patton <jenbuckleypatton@gmail.com>
2023-09-21 19:23:57 -0700

    installing phosphor-react and starting navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+//import here
+import React from "react";
+
+function NavBar() {
+    return (
+        <div className="navbar">
+            <div className="links">
+                <Link to="/">Shop</Link>
+                <Link to=""></Link>
+                <Link to="/cart">Cart</Link>
+            </div>
+        </div>
+
+    );
+}
