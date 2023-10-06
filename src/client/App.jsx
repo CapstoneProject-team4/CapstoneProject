079ac130296af20820ca2aa5b2158e4251275896
Jen Patton <132628955+Jenbpatton@users.noreply.github.com>
2023-10-05 18:09:58 -0700

    getting rid of the App.js, but keeping the App.jsx

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -11,6 +11,16 @@ function App() {
         <img id='comp-img' src='./computer.png'></img>
         <p>Replace the starter code in this template with something cool</p>
         <Login />
+        <div className="Routes">
+            <Router>
+            <Routes>
+                <Route path= />
+                <Route path= />
+                <Route path= />
+            </Routes>
+            </Router>
+        </div>
+    );
     </div>
   );
 }
