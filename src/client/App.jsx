9604f5573dd3f3a74612929ee15ec462938b3c9f
Jen Patton <132628955+Jenbpatton@users.noreply.github.com>
2023-10-10 11:11:47 -0700

    creating Routes using react-router-dom

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,19 +1,26 @@
 
-import Product from "./Pages/Product";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import Product from "./Pages/Product";
 import ProductList from "./Pages/ProductList";
-import Register from "./pages/Register";
+import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import Cart from "./Pages/Cart";
 
-
-
-
-
-
-
 function App() {
-  return <Home/>
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/product/:id" component={Product} />
+        <Route path="/products" component={ProductList} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/cart" component={Cart} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
