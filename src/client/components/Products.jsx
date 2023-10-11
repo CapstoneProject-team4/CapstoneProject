f92309ebf1fd3f59e02698136d98dad2d36cb66f
andreeamerloiu <82194090+andreeamerloiu@users.noreply.github.com>
2023-10-10 20:40:17 -0700

    Update Products.jsx

--- src/client/components/Products.jsx
+++ src/client/components/Products.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components"
-import { popularProducts } from "../data"
-import Product from "./Product"
+
 
 const Container = styled.div`
 padding: 20px;
@@ -8,15 +7,52 @@ padding: 20px;
     flex-wrap: wrap;
     justify-content: space-between;
 `;
-
-const Products = () => {
-  return (
-   <Container>
-   {popularProducts.map((item) => (
-    <Product item={item} key={item.id} />
-   ))}
-   </Container>
-  )
+export async function fetchAllProducts() {
+  try {
+    const response = await fetch(
+    'https://localhost:3000/api/products'
+    );
+    const result = await response.json();
+    console.log("what",result.products);
+    return result.products;
+    
+  
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+function products() {
+  return products.map((products) => {
+      return (
+          <div key={products.id} className="players">
+          <h2>{products.title}</h2>
+          <h4>{products.price}</h4>
+          <h4>{player.description}</h4>
+          <img src={products.img}/>
+          <button onClick = {() => navigate(`/players/${player.id}`)}>See Details</button>
+          
+      </div>
+      )
+  });
 }
 
-export default Products
+useEffect(()=>{
+  async function allProductsHandler(){
+      const result = await fetchAllProducts()
+      setPlayers(result)
+      console.log("2",result)
+
+  }
+  allProductsHandler()
+
+},[]
+)
+return (
+  <div>
+       {renderAllProducts()}
+  </div>
+ 
+  )
+
+export default products
