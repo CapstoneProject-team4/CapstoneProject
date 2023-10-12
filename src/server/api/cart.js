6b72cd4b6a030a3d7930802326679b7db1714cc6
Jason Chen <sollenigma@gmail.com>
2023-10-11 19:21:06 -0700

    fixing a few lines

--- src/server/api/cart.js
+++ src/server/api/cart.js
@@ -9,7 +9,7 @@ cartRouter.post('/:productID/:quantity', async( req, res, next) => {
       const {productID} = req.params;
       const {quantity} = req.params;
       res.cookie( productID, quantity);
-      // res.send("data is stored in cookies");
+      res.send(res.cookie);
   } catch (error) {
      console.log(error);
       next(error)
@@ -33,7 +33,7 @@ cartRouter.delete('/', async( req, res, next) => {
         currentcookie=cartdata[i].substring(0, cartdata[i].search("="))
         currentcookie=currentcookie.replace(/\s/g, '');
         res.clearCookie(currentcookie);
-        // res.send(currentcookie);
+        res.send(currentcookie);
       }
       // res.send("Cookies are cleared!");
   } catch (error) {
@@ -41,56 +41,5 @@ cartRouter.delete('/', async( req, res, next) => {
       next(error)
   }
 });
-
-
-
-
-module.exports = cartRouter;
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const cartRouter = express.Router();
-cartRouter.use(cookieParser());
-
-cartRouter.post('/:productID/:quantity', async( req, res, next) => {
-  try {
-      // no: set a new cookie
-      const {productID} = req.params;
-      const {quantity} = req.params;
-      res.cookie( productID, quantity);
-      // res.send("data is stored in cookies");
-  } catch (error) {
-     console.log(error);
-      next(error)
-  }
-});
-
-cartRouter.get('/', async( req, res, next) => {
-  try {
-      res.send(req.headers.cookie);
-      console.log(req.cookies);
-  } catch (error) {
-     console.log(error);
-      next(error)
-  }
-});
-
-cartRouter.delete('/', async( req, res, next) => {
-  try {
-      cartdata = req.headers.cookie.split("; ");
-      for (i=0; i<cartdata.length; i++){
-        currentcookie=cartdata[i].substring(0, cartdata[i].search("="))
-        currentcookie=currentcookie.replace(/\s/g, '');
-        res.clearCookie(currentcookie);
-        // res.send(currentcookie);
-      }
-      // res.send("Cookies are cleared!");
-  } catch (error) {
-     console.log(error);
-      next(error)
-  }
-});
-
-
-
 
 module.exports = cartRouter;
